10826e58475d55538f5b505fc1e6495af145fcd4
Rachelooo <91844200+Rachelooo@users.noreply.github.com>
2023-01-21 10:34:01 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ classifier=ml5.imageClassifier("DoodleNet");
 function setup(){
 
 canvas = createCanvas(400, 400);
-canvas.position(600, 600);
+canvas.position(550, 300);
 background("white");
 synth=window.speechSynthesis;
 canvas.mouseReleased(classifyCanvas);
@@ -27,13 +27,11 @@ function clearCanvas(){
 function classifyCanvas(){
     classifier.classify(canvas,gotResult);
 }
-function gotResult(results, error){
+function gotResult(error, results){
 if(error){
     console.error(error);
 }
-else{
     console.log(results);
-}
 document.getElementById("label").innerHTML="label - "+ results[0].label;
 document.getElementById("confidence").innerHTML="confidence - "+ Math.round(results[0].confidence * 100) +"%";
 utterThis= new SpeechSynthesisUtterance(results[0].label);
